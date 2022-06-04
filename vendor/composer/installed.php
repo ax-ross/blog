ef6d3455b8ffaa7c173a3f91e036fb44ac615a3f
ax-ross <ax-ross@yandex.ru>
2022-06-04 16:15:17 +0300

    changed local

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4655cb585e06114566449e507b5eaa1acffab67f',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4655cb585e06114566449e507b5eaa1acffab67f',
+            'dev_requirement' => false,
+        ),
+        'laravel-lang/lang' => array(
+            'pretty_version' => '10.9.1',
+            'version' => '10.9.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../laravel-lang/lang',
+            'aliases' => array(),
+            'reference' => '07e100bbe05a876baed68b2dab12742e4f705ec8',
+            'dev_requirement' => true,
+        ),
+    ),
+);
