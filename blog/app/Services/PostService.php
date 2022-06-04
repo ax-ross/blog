e2df8b5e6f538d72201fca47434923e54c2fc776
ax-ross <ax-ross@yandex.ru>
2022-06-04 16:27:49 +0300

    fixed undefined tag_ids key error

--- blog/app/Services/PostService.php
+++ blog/app/Services/PostService.php
@@ -11,24 +11,33 @@ class PostService
     public function store($data)
     {
         DB::transaction(function () use ($data) {
-            $tag_ids = $data['tag_ids'];
-            unset($data['tag_ids']);
+            if (isset($data['tag_ids'])) {
+                $tag_ids = $data['tag_ids'];
+                unset($data['tag_ids']);
+            }
+
             $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
             $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
             $post = Post::create($data);
-            $post->tags()->attach($tag_ids);
+            if (isset($data['tag_ids'])) {
+                $post->tags()->attach($tag_ids);
+            }
         });
     }
 
     public function update($post, $data)
     {
         DB::transaction(function () use ($post, $data) {
-            $tag_ids = $data['tag_ids'];
-            unset($data['tag_ids']);
+            if (isset($data['tag_ids'])) {
+                $tag_ids = $data['tag_ids'];
+                unset($data['tag_ids']);
+            }
             !(isset($data['preview_image'])) || (($data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image'])));
             !(isset($data['main_image'])) || ($data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']));
             $post->update($data);
-            $post->tags()->sync($tag_ids);
+            if (isset($data['tag_ids'])) {
+                $post->tags()->sync($tag_ids);
+            }
         });
     }
 }
