5946797b5ffaf3163ce3988bdddc4a0a48e0dfcc
ax-ross <ax-ross@yandex.ru>
2022-06-07 17:07:55 +0300

    added admin middleware

--- blog/resources/views/admin/layouts/main.blade.php
+++ blog/resources/views/admin/layouts/main.blade.php
@@ -40,11 +40,19 @@
     </div>
 
     <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light justify-content-between">
         <!-- Left navbar links -->
         <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+        </ul>
+        <ul class="navbar-nav p-3">
             <li class="nav-item">
-                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                <form action="{{ route('logout') }}" method="post">
+                    @csrf
+                    <input type="submit" value="Выйти" class="btn btn-primary">
+                </form>
             </li>
         </ul>
     </nav>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\IndexController::class, 'index'])->name('index.page');
 
-Route::prefix('admin')->name('admin.')->group(function () {
+Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('/', [\App\Http\Controllers\Admin\IndexController::class, 'index'])->name('index');
     Route::resource('categories', \App\Http\Controllers\Admin\CategoriesController::class);
     Route::resource('tags', \App\Http\Controllers\Admin\TagsController::class);
