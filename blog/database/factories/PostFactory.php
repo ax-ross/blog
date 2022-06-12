97feed12bbfbd8d7e76e4ccdba049bc47753f288
ax-ross <ax-ross@yandex.ru>
2022-06-12 21:05:22 +0300

    added factory for posts

--- blog/database/factories/PostFactory.php
+++ blog/database/factories/PostFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,8 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title' => $this->faker->sentence(5),
+            'content' => $this->faker->text(300),
         ];
     }
 }
