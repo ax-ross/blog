05d9118f83f2228c1d746719d662cdad765e97c1
ax-ross <ax-ross@yandex.ru>
2022-06-13 18:22:32 +0300

    implemented index action of blog controller

--- blog/app/Http/Controllers/BlogController.php
+++ blog/app/Http/Controllers/BlogController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::paginate(12);
+        $randomPosts = Post::get()->random(4);
+        $mostLikedPosts = Post::withCount('likesFromUsers')->orderBy('likes_from_users_count', 'DESC')->get()->take(4);
+        return view('blog', compact('posts', 'randomPosts', 'mostLikedPosts'));
+    }
 }
