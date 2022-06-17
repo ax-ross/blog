6233414d18eeea12305a99ed0f6c5b6796e11c96
ax-ross <ax-ross@yandex.ru>
2022-06-17 10:45:38 +0300

    refactored

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -17,26 +17,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\IndexController::class, 'index'])->name('index.page');
 Route::get('/posts', [\App\Http\Controllers\PostController::class, 'index'])->name('posts.index');
 Route::get('posts/{post}', [\App\Http\Controllers\PostController::class, 'show'])->name('posts.show');
-Route::resource('posts.comments', \App\Http\Controllers\PostCommentController::class)->only('store')->middleware(['auth', 'verified']);
-Route::prefix('{post}/likes')->group(function() {
-    Route::post('/', [\App\Http\Controllers\PostLikeController::class, 'store'])->name('posts.likes.store');
-});
 
-Route::middleware(['auth', 'verified'])->prefix('personal')->name('personal.')->group(function () {
-    Route::get('/', [\App\Http\Controllers\Personal\IndexController::class, 'index'])->name('index');
-    Route::get('liked-posts', [\App\Http\Controllers\Personal\LikedPostController::class, 'index'])->name('liked-posts.index');
-    Route::prefix('liked-posts')->name('liked-posts.')->group(function () {
-        Route::get('/', [\App\Http\Controllers\Personal\LikedPostController::class, 'index'])->name('index');
-        Route::delete('/{post}', [\App\Http\Controllers\Personal\LikedPostController::class, 'destroy'])->name('destroy');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::prefix('personal')->name('personal.')->group(function () {
+        Route::get('/', [\App\Http\Controllers\Personal\IndexController::class, 'index'])->name('index');
+        Route::get('liked-posts', [\App\Http\Controllers\Personal\LikedPostController::class, 'index'])->name('liked-posts.index');
+        Route::prefix('liked-posts')->name('liked-posts.')->group(function () {
+            Route::get('/', [\App\Http\Controllers\Personal\LikedPostController::class, 'index'])->name('index');
+            Route::delete('/{post}', [\App\Http\Controllers\Personal\LikedPostController::class, 'destroy'])->name('destroy');
+        });
+        Route::prefix('comments')->resource('comments', \App\Http\Controllers\Personal\CommentController::class)->except(['create', 'store']);
     });
-    Route::prefix('comments')->resource('comments', \App\Http\Controllers\Personal\CommentController::class)->except(['create', 'store']);
+
+    Route::post('{post}/comments', [\App\Http\Controllers\PostCommentController::class, 'store'])->name('posts.comments.store');
+    Route::post('{post}/likes', [\App\Http\Controllers\PostLikeController::class, 'store'])->name('posts.likes.store');
+
+    Route::middleware('admin')->prefix('admin')->name('admin.')->group(function () {
+        Route::get('/', [\App\Http\Controllers\Admin\IndexController::class, 'index'])->name('index');
+        Route::resource('categories', \App\Http\Controllers\Admin\CategoryController::class);
+        Route::resource('tags', \App\Http\Controllers\Admin\TagController::class);
+        Route::resource('posts', \App\Http\Controllers\Admin\PostController::class);
+        Route::resource('users', \App\Http\Controllers\Admin\UserController::class);
+    });
 });
 
-Route::middleware(['auth', 'admin', 'verified'])->prefix('admin')->name('admin.')->group(function () {
-    Route::get('/', [\App\Http\Controllers\Admin\IndexController::class, 'index'])->name('index');
-    Route::resource('categories', \App\Http\Controllers\Admin\CategoryController::class);
-    Route::resource('tags', \App\Http\Controllers\Admin\TagController::class);
-    Route::resource('posts', \App\Http\Controllers\Admin\PostController::class);
-    Route::resource('users', \App\Http\Controllers\Admin\UserController::class);
-});
 Auth::routes(['verify' => true]);
